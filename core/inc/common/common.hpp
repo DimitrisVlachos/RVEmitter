ba4738f0092a995fc17a158e2877f4cfcaef3f4c
DimitrisVlachos <dvlachos2k@gmail.com>
2022-05-06 02:58:23 +0200

    Common: Remove unused includes

--- core/inc/common/common.hpp
+++ core/inc/common/common.hpp
@@ -14,22 +14,14 @@
 */
 
 #pragma once
-
 #include <cstdint>
 #include <iostream>
-#include <vector>
-#include <atomic>
 #include <cassert>
 #include <fstream>
 #include <string>
 #include <memory>
 #include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <functional>
-#include <thread>
-#include <mutex>
-#include <array>
 #include <fmt/format.h>
 #include <type_traits>
 #include <algorithm>
+#include <vector>
